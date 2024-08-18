a991199b78645959e5157c292622d58c95c30f73
SanthoshKumar-Ravi <krpsanthoshkumar@gmail.com>
2024-08-18 11:12:13 +0300

    Adding git ignore

---
+++ target/generated-sources/openapi/src/main/java/com/example/readreplicapoc/api/model/Person.java
@@ -0,0 +1,179 @@
+package com.example.readreplicapoc.api.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * Person
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-08-12T16:12:14.444576+03:00[Asia/Riyadh]")
+public class Person {
+
+  @JsonProperty("id")
+  private Integer id = null;
+
+  @JsonProperty("middleName")
+  private String middleName;
+
+  @JsonProperty("firstName")
+  private String firstName;
+
+  @JsonProperty("lastName")
+  private String lastName;
+
+  @JsonProperty("age")
+  private Integer age;
+
+  public Person id(Integer id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  */
+  
+  @Schema(name = "id", example = "1", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Person middleName(String middleName) {
+    this.middleName = middleName;
+    return this;
+  }
+
+  /**
+   * Get middleName
+   * @return middleName
+  */
+  
+  @Schema(name = "middleName", example = "Doe", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+  public Person firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  /**
+   * Get firstName
+   * @return firstName
+  */
+  
+  @Schema(name = "firstName", example = "John", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public Person lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  /**
+   * Get lastName
+   * @return lastName
+  */
+  
+  @Schema(name = "lastName", example = "Mike", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Person age(Integer age) {
+    this.age = age;
+    return this;
+  }
+
+  /**
+   * Get age
+   * @return age
+  */
+  
+  @Schema(name = "age", example = "10", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Person person = (Person) o;
+    return Objects.equals(this.id, person.id) &&
+        Objects.equals(this.middleName, person.middleName) &&
+        Objects.equals(this.firstName, person.firstName) &&
+        Objects.equals(this.lastName, person.lastName) &&
+        Objects.equals(this.age, person.age);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, middleName, firstName, lastName, age);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Person {\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    middleName: ").append(toIndentedString(middleName)).append("\n");
+    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
+    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
+    sb.append("    age: ").append(toIndentedString(age)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
